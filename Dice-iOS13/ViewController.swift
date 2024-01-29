477ddb6062b5885a82c158beba435c50c436a8c5
Christian Jayme <Chrisian.jayme26@gmail.com>
2024-01-29 13:03:22 +0800

    Initial Commit

---
+++ Dice-iOS13/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dice-iOS13
+//
+//  Created by Christian jayme on 1/29/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- Dice-iOS13Tests/Dice_iOS13Tests.swift
+++ Dice-iOS13Tests/Dice_iOS13Tests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+@testable import Dice_iOS13
 
 final class Dice_iOS13Tests: XCTestCase {
 
@@ -27,7 +28,7 @@ final class Dice_iOS13Tests: XCTestCase {
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        measure {
+        self.measure {
             // Put the code you want to measure the time of here.
         }
     }
