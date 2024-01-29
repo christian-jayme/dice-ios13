8104e897ac1d086cac0f728c64fbc68ce52fa22b
Christian Jayme <Chrisian.jayme26@gmail.com>
2024-01-29 11:00:03 +0800

    Initial Commit

---
+++ Dice-iOS13/Dice_iOS13App.swift
@@ -0,0 +1,17 @@
+//
+//  Dice_iOS13App.swift
+//  Dice-iOS13
+//
+//  Created by Christian jayme on 1/29/24.
+//
+
+import SwiftUI
+
+@main
+struct Dice_iOS13App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
